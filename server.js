37c84a5ba00243899556403fc3a850c7873d63f2
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-05-17 22:00:46 +0200

    Set for production

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const dotnev = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
@@ -18,6 +19,14 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use("/api/v1/transactions", transactions);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
